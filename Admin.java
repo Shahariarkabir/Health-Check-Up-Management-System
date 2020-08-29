9a2c4e6297a83ddd4358899fa449a00c7f0edbdd
shahariarkabir <shahariar7days@gmail.com>
2020-08-29 14:44:01 +0600

    Admin class update

--- Admin.java
+++ Admin.java
@@ -1,3 +1,14 @@
 public class Admin {
-    
+ // Admin class to create an admin for login
+public class Admin{
+    // variables  
+     int id;
+    String name;
+    // Constructor to initialize the variables
+    public Admin(int i, String n)
+    {
+        id = i;
+        name = n;
+    }
+}   
 }
--- Patient.java
+++ Patient.java
@@ -1,15 +1,12 @@
 public class Patient {
-    
-public static void main (String[] args) 
-{
-    System.out.println ("I AM BLESSED TO HAVE YOU IN MY.....Java project group ");
 
+    public static void main(String[] args) {
+        System.out.println("I AM BLESSED TO HAVE YOU IN MY.....Java project group ");
+
+    }
 }
-}
 
-
-
-// git pull 
-//  git add . - code gulo  add hoar jonno
+// git pull
+// git add . - code gulo add hoar jonno
 // git commit-m " comment"
-//  git push
+// git push
